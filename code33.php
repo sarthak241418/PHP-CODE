e4b6d180e28f61f421ef81d17067f16eb146911e
sarthak241418 <146878086+sarthak241418@users.noreply.github.com>
2025-02-11 23:49:15 +0530

    Add files via upload

---
+++ code33.php
@@ -0,0 +1,12 @@
+<?php
+
+    	function writeMessage()
+    	{
+        	echo "Hello! Manshay this side.";
+    	}
+    
+    	writeMessage();
+	
+	echo "<br><br>";
+	echo "Code executed by Manshay(0221BCA106)";    
+?>
---
+++ code40.php
@@ -0,0 +1,11 @@
+<?php
+	function factorial($n) {
+    		if ($n == 0 || $n == 1) return 1; // Base case
+    		return $n * factorial($n - 1); // Recursive call
+	}
+
+	$number = 5;
+	echo "Factorial of $number is: " . factorial($number);
+	echo "<br><br>";
+	echo "Code executed by Manshay(0221BCA106)";
+?>
